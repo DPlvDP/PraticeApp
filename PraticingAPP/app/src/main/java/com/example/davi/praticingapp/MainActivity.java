3dbe51cb50baa17427a73b4fad416b302d483bcd
DPlvDP <davikniest2001@gmail.com>
2018-11-15 22:45:02 -0200

    App Recipes

--- PraticingAPP/app/src/main/java/com/example/davi/praticingapp/MainActivity.java
+++ PraticingAPP/app/src/main/java/com/example/davi/praticingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.davi.praticingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openList(View view) {
+        Intent StartNewActivity = new Intent(this,Recipes.class );
+        startActivity(StartNewActivity);
+    }
+
 }
